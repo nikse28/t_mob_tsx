8652119b6778007971b2fba8bd8039b20318a592
nikse28 <nikse28@gmail.com>
2019-06-04 09:09:38 +0530

    edit table

--- src/components/ReconRules.tsx
+++ src/components/ReconRules.tsx
@@ -1,98 +1,105 @@
-import React, { Component } from 'react'
-import { Select ,Button ,Input  } from "antd";
-import TableRules from './TableRules';
+import React, { Component } from "react";
+import { Select, Button, Input } from "antd";
+import TableRules from "./TableRules";
 import dataJson from "../data/data";
-import '../index.css'
+import "../index.css";
 
 const { TextArea } = Input;
 export default class ReconRules extends Component {
-    
-    state= {
-        addRuleTable:[
-          {"id":1,"name":"John"}
-        ],
-        sources : dataJson.sourceData,
-        records : dataJson.recordData,
-        entity :  dataJson.entityData,
-    }
-      filterRecord :any= [];
-      filterEntityData :any = [];  
-      
-    handleSourceChange=(e:any)=> {
-        // console.log('Source  ',e.target.value);
-        this.filterRecord = [];
-        this.state.records.filter(data => {
-        if (e === data.sourceId) {
-            this.filterRecord.push(data);
-          }
-        });
-        this.setState({  });
-    }
-    handleRecordChange=(e:any)=> {
-        console.log('ERTO',e);
-        this.filterEntityData = [];
-        this.state.entity.filter((data:any) => {
-          if (e === data.recordId) {
-            console.log('Entity data...',data);
-            this.filterEntityData.push(data);
-          }
-        });
-    }
-    
-    handleAddRules=()=>{
-        const newState = {"id":2,"name":"KASDIJ"};
-        this.setState({
-            addRuleTable:[
-                ...this.state.addRuleTable,
-                newState
-            ] 
-        });
-    }
-    handleRemoveRule=()=>{
-        this.state.addRuleTable.pop();
-        this.setState({});
-    }
+  state = {
+    addRuleTable: [{ id: 1, name: "John" }],
+    sources: dataJson.sourceData,
+    records: dataJson.recordData,
+    entity: dataJson.entityData
+  };
+  filterRecord: any = [];
+  filterEntityData: any = [];
 
-    render() {
-        const { Option } = Select;
-        return (
-            <div>
-                Source
-                <Select  id="1" onChange={this.handleSourceChange} className="rec-select-box">
-                { this.state.sources.map (res=>{
-                    return(                      
-                        <Option value={res.sourceId} key={res.sourceId}>{res.sourceName}</Option>
-                    )
-                })}
-                </Select>
-                rules
-                <Select id="2" onChange={this.handleRecordChange} className="rec-select-box">
-                    {
-                        this.filterRecord.map((res:any)=>{
-                            return(
-                                <Option value={res.recordId} key={res.recordId}>{res.recordName}</Option>
-                            )
-                        })
-                    }
-                </Select>
+  handleAddRules = () => {
+    const newState = { id: 2, name: "KASDIJ" };
+    this.setState({
+      addRuleTable: [...this.state.addRuleTable, newState]
+    });
+  };
 
-                <br/><br/>
-               <div>
-                 Notes:
-                 </div> 
-                <TextArea rows={4} className="rec-text-area"/> <br/><br/>
-                <Button type="ghost" onClick={this.handleAddRules}>Add Rule</Button>  <Button onClick={this.handleRemoveRule}>Remove Rule</Button><br/><br/>
-                {
-                    this.state.addRuleTable.map(res=>{
-                        return(
-                            <div key={Math.random()}>
-                                <TableRules/>
-                                <br/>
-                            </div>
-                        )
-                    })
-                }
+  handleSourceChange = (e: any) => {
+    // console.log('Source  ',e.target.value);
+    this.filterRecord = [];
+    this.state.records.filter(data => {
+      if (e === data.sourceId) {
+        this.filterRecord.push(data);
+      }
+    });
+    this.setState({});
+  };
+  handleRecordChange = (e: any) => {
+    console.log("ERTO", e);
+    this.filterEntityData = [];
+    this.state.entity.filter((data: any) => {
+      if (e === data.recordId) {
+        console.log("Entity data...", data);
+        this.filterEntityData.push(data);
+      }
+    });
+  };
+
+  handleRemoveRule = () => {
+    this.state.addRuleTable.pop();
+    this.setState({});
+  };
+
+  render() {
+    const { Option } = Select;
+    return (
+      <div>
+        Source
+        <Select
+          id="1"
+          onChange={this.handleSourceChange}
+          className="rec-select-box"
+        >
+          {this.state.sources.map(res => {
+            return (
+              <Option value={res.sourceId} key={res.sourceId}>
+                {res.sourceName}
+              </Option>
+            );
+          })}
+        </Select>
+        rules
+        <Select
+          id="2"
+          onChange={this.handleRecordChange}
+          className="rec-select-box"
+        >
+          {this.filterRecord.map((res: any) => {
+            return (
+              <Option value={res.recordId} key={res.recordId}>
+                {res.recordName}
+              </Option>
+            );
+          })}
+        </Select>
+        <br />
+        <br />
+        <div>Notes:</div>
+        <TextArea rows={4} className="rec-text-area" /> <br />
+        <br />
+        <Button type="ghost" onClick={this.handleAddRules}>
+          Add Rule
+        </Button>{" "}
+        <Button onClick={this.handleRemoveRule}>Remove Rule</Button>
+        <br />
+        <br />
+        {this.state.addRuleTable.map(res => {
+          return (
+            <div key={Math.random()}>
+              <TableRules />
+              <br />
             </div>
-        )
-    }
+          );
+        })}
+      </div>
+    );
+  }
 }
